2aa1898dc88651b49dcb999fd264052c54ccec5e
Minh Bùi <bminh248@gmail.com>
2025-04-09 08:41:00 +0700

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by Bùi Minh on 9/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProfileTests/ProfileTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileTests.swift
+//  ProfileTests
+//
+//  Created by Bùi Minh on 9/4/25.
+//
+
+import Testing
+@testable import Profile
+
+struct ProfileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
